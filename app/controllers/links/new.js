314625278593dd95d820f287adbe820b3fd7f5a0
Chase Clettenberg <clettenberg@gmail.com>
2015-10-10 11:29:36 -0500

    Fixed JS Lint errors

--- app/controllers/links/new.js
+++ app/controllers/links/new.js
@@ -3,24 +3,24 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
     createLink: function(link){
-      var store = this.store
-      var title = link.title === undefined ? "" : link.title
-      var description = link.description === undefined ? "" : link.description
+      var store = this.store;
+      var title = link.title === undefined ? "" : link.title;
+      var description = link.description === undefined ? "" : link.description;
       var new_link = store.createRecord('link', {
         url: link.url,
         title: title,
         description: description
-      })
+      });
 
       var self = this;
-      function transitionToIndex(link) {
+      function transitionToIndex() {
         self.transitionToRoute('index');
       }
 
       function failure(reason) {
         // handle the error
 
-        Ember.Logger.error("Failed to reroute");
+        Ember.Logger.error("Failed to reroute beacuse of" + reason);
       }
 
       new_link.save().then(transitionToIndex).catch(failure);
